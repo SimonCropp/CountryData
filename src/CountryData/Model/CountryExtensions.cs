174b774c9d40debb919c03583ff4ce51bd6ab4d9
Simon Cropp <simon.cropp@gmail.com>
2019-08-13 08:46:30 +1000

    Create CountryExtensions.cs

---
+++ src/CountryData/Model/CountryExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CountryData
+{
+    public static class CountryExtensions
+    {
+        public static IEnumerable<IProvince> Provinces(this ICountry country)
+        {
+            Guard.AgainstNull(country, nameof(country));
+            return country.States.SelectMany(x => x.Provinces);
+        }
+
+        public static IEnumerable<ICommunity> Communities(this ICountry country)
+        {
+            Guard.AgainstNull(country, nameof(country));
+            return country.Provinces().SelectMany(x => x.Communities);
+        }
+
+        public static IEnumerable<IPlace> Places(this ICountry country)
+        {
+            Guard.AgainstNull(country, nameof(country));
+            return country.Communities().SelectMany(x => x.Places);
+        }
+    }
+}
