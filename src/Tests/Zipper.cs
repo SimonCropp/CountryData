2eb071be2290aae3aef34b933a59fe4afc3c6465
Simon Cropp <simon.cropp@gmail.com>
2020-12-07 13:14:49 +1100

    Update Zipper.cs

--- src/Tests/Zipper.cs
+++ src/Tests/Zipper.cs
@@ -8,12 +8,12 @@ static class Zipper
     {
         File.Delete(targetFile);
         using var zipStream = File.Create(targetFile);
-        using var archive = new ZipArchive(zipStream, ZipArchiveMode.Create);
+        using ZipArchive archive = new(zipStream, ZipArchiveMode.Create);
         foreach (var file in Directory.EnumerateFiles(sourceDirectory))
         {
             var entry = archive.CreateEntry(Path.GetFileName(file));
             //To stop the zip changing from the perspective of git
-            entry.LastWriteTime = new DateTimeOffset(2000, 1, 1, 1, 1, 1, TimeSpan.Zero);
+            entry.LastWriteTime = new(2000, 1, 1, 1, 1, 1, TimeSpan.Zero);
 
             using var fileStream = File.OpenRead(file);
             using var entryStream = entry.Open();
